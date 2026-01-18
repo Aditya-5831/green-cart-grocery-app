c8eafdc9009089c954880ae1112611c83e3a70d4
Aditya Shiyale <adityashiyale7219@gmail.com>
2026-01-18 18:41:44 +0530

    Initial frontend setup: React project created

---
+++ frontend/src/context/AppContext.tsx
@@ -0,0 +1,48 @@
+import { createContext, type ReactNode, useContext, useState } from "react";
+
+import { useNavigate, type NavigateFunction } from 'react-router-dom';
+
+interface User {
+    id: string;
+    name: string;
+    email?: string
+}
+
+interface AppContextType {
+    navigate: NavigateFunction;
+    user: User | null,
+    setUser: (user: User | null) => void;
+    isSeller: boolean;
+    setIsSeller: (isSeller: boolean) => void;
+}
+
+type ProviderProps = {
+    children: ReactNode
+}
+
+export const AppContext = createContext<AppContextType | null>(null);
+
+export const AppContextProvider = ({ children }: ProviderProps) => {
+    const navigate = useNavigate()
+    const [user, setUser] = useState<User | null>(null)
+    const [isSeller, setIsSeller] = useState<boolean>(false)
+
+    const value: AppContextType = {
+        navigate,
+        user,
+        setUser,
+        isSeller,
+        setIsSeller
+    };
+    return <AppContext.Provider value={value}>
+        {children}
+    </AppContext.Provider>
+}
+
+export const useAppContext = () => {
+    const context = useContext(AppContext)
+
+    if (!context) throw new Error("useAppContext must be used within AppContextProvider");
+
+    return context;
+}
